6b290682baed26276afb96b007eeabde17145a47
RockyPacks <rockychueu21@gmail.com>
2026-03-14 17:23:29 +0200

    Add Contact section and Section component

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Experience from './components/Experience';
 import Education from './components/Education';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
 import SponsoringOrgs from './components/SponsoringOrgs';
 import Chatbot from './components/Chatbot';
@@ -148,6 +149,7 @@ const App: React.FC = () => {
         <SponsoringOrgs />
       </>
     ),
+    '#contact': <Contact />,
   };
 
   return (
